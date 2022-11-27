13867438999f312f1aa4f6dc680825d998c87584
Daniel Cook <Dcook242424@gmail.com>
2022-11-27 08:58:10 -0800

    Adding search needs leaveoneout calcs

--- Dcook_Cs170_project2/Dcook_Cs170_project2.cpp
+++ Dcook_Cs170_project2/Dcook_Cs170_project2.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 #include <sstream> 
+#include <random>
+
 
 using namespace std;
 
@@ -27,16 +29,62 @@ int main()
         data.push_back(row);                //pushes row into data
     }
 
-    //cout << data.size() << endl;
-    //cout << data[0].size() << endl;
-    for (int i = 0; i < data.size(); i++) {
-        for (int e = 0; e < data[0].size(); e++) {          //assumes that all vectors are same size could cause errors
-            cout << data[i][e] << " ";
+
+    vector<int> current_set = {};
+    int bestsetsize = 0;
+    double bestacc = 0;
+    double accuracy;
+    int k = 0;
+
+    for (int i = 1; i < data[0].size(); i++) {
+        cout << "On the " << i << "th level of search tree\n";
+        int feature_to_add;
+        double best_so_far = 0;
+
+        for (k = 1; k < data[0].size(); k++) {
+            cout << "Consider adding " << k << " feature\n";
+
+            if (!((find(current_set.begin(), current_set.end(), k) != current_set.end()))) {
+
+                accuracy = (rand()%99);
+
+                    if (accuracy > best_so_far) {
+                        best_so_far = accuracy;
+                            feature_to_add = k;
+                    }
+                    cout << "Accuracy = " << accuracy << endl;
+            }
+
         }
-        cout << endl;
+
+        current_set.push_back(feature_to_add);
+        cout << "On level " << i <<  " added feature " <<  feature_to_add << " to set\n";
+        if (best_so_far > bestacc) {
+            bestacc = best_so_far;
+            bestsetsize = i;
+        }
     }
 
+    cout << "set = { ";
+    for (int i = 0; i < bestsetsize; i++) {
+        cout << current_set[i] << " ";
+    }
 
-    std::cout << "Hello World!\n";
+    cout << "}\nwih an accuracy of " << bestacc;
+
+    //cout << data.size() << endl;
+    //cout << data[0].size() << endl;
+    //for (int i = 0; i < data.size(); i++) {
+    //    for (int e = 0; e < data[0].size(); e++) {          //assumes that all vectors are same size could cause errors
+    //        cout << data[i][e] << " ";
+    //    }
+    //    cout << endl;
+    //}
+
+
+
+
+
+   
 }
 
