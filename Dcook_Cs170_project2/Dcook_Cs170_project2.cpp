be6392c6057a33091890546c6f06bfc16d76ee45
Daniel Cook <Dcook242424@gmail.com>
2022-12-08 01:14:56 -0800

    Adding comments and fixing formating for outputs

--- Dcook_Cs170_project2/Dcook_Cs170_project2.cpp
+++ Dcook_Cs170_project2/Dcook_Cs170_project2.cpp
@@ -8,6 +8,7 @@
 #include <random>
 #include <cmath>
 #include <chrono>
+#include <iomanip>
 
 using namespace std;
 using namespace std::chrono;        //needed for timer
@@ -15,19 +16,21 @@ using namespace std::chrono;        //needed for timer
 int main()
 {
 
-    int algorithm = 3;
+    int algorithm = 3;              //holds what algorithm to use 1 or 2
     while ((algorithm != 1) && (algorithm != 2)) {
     cout << "Forward Selection  (1)\nBackward Elimination  (2)\n\nWhich Algorithm would you like to run?:";
     cin >> algorithm;
     if ((algorithm != 1) && (algorithm != 2)) {
-        cout << "invalid input choose 1 or 2\n\n";
+        cout << "invalid input choose 1 or 2\n\n";                  //takes which algorithm to use
     }
 }
+    cout << fixed;                  //sets how accurate our output values will be .00
+    cout << setprecision(1);
     string line;
     double num = 0.0;
-    ifstream infile("input_large.txt");
-    vector<vector<double>> data;
-    while (getline(infile, line)) {
+    ifstream infile("input_large.txt");             //input file
+    vector<vector<double>> data;                    //holds data 2d vector
+    while (getline(infile, line)) {                 //fills data with file
         vector<double> row;
         stringstream ss(line);
         while ((ss.peek())!=EOF) {
@@ -37,16 +40,16 @@ int main()
         data.push_back(row);                //pushes row into data
     }
 
-    vector<int> solution = {};
-    vector<int> current_set = {};
+    vector<int> solution = {};              //holds the best solution 
+    vector<int> current_set = {};           //holds current chosen set
 
     if (algorithm == 2) {
-        for (int e = 1; e < data[0].size(); e++) {
+        for (int e = 1; e < data[0].size(); e++) {  //fills current_set with all 
             current_set.push_back(e);
         }
     }
     cout << "set = { ";
-    for (int i = 0; i < current_set.size(); i++) {
+    for (int i = 0; i < current_set.size(); i++) {  //outputs what the set is at start debugging and whatnot
         cout << current_set[i] << " ";
     }
     cout << " }\n";
@@ -61,14 +64,14 @@ int main()
 
     auto start = high_resolution_clock::now();                       //starts timer
 
-    for (int i = 1; i < size; i++) {
+    for (int i = 1; i < size; i++) {                                //goes though whole whole set -- on elimination to avoid breaking vector
         cout << "On the " << i << "th level of search tree\n";
-        int feature_to_add;
-        double best_so_far = -1;
+        int feature_to_add;                                         //the location of the feature to add or remove
+        double best_so_far = -1;                                    //best choice value
 
-        for (k = 1; k < data[0].size(); k++) {
+        for (k = 1; k < data[0].size(); k++) {                      //goes though set taking k
            
-            bool check;
+            bool check;                                             //for forward checks that k is not in set for eliminaiton checks k is in set
 
             if (algorithm == 1) {
                 check = !((find(current_set.begin(), current_set.end(), k) != current_set.end()));
@@ -78,23 +81,23 @@ int main()
             }
             
 
-            if (check) { 
+            if (check) {                                            //check
                 //cout << "Consider adding " << k << " feature\n";
-                int number_correctly_classfied = 0;
+                int number_correctly_classfied = 0;                 //number that was correct
 
-                for (int c = 0; c < data.size(); c++) {
-                    double nearest_neighbor_distance = 5000000;
+                for (int c = 0; c < data.size(); c++) {             //calculates accuracy 
+                    double nearest_neighbor_distance = 5000000;     //just large number to start
                     int nearest_neighbor_location = 5000000;
                     double nearest_neighbor_label;
-                    double lable_object_to_classify = data[c][0];
+                    double lable_object_to_classify = data[c][0];   //type
                     
-                    for (int d = 0; d < data.size(); d++) {
+                    for (int d = 0; d < data.size(); d++) {         //searches for nearest
 
                         if (c != d) {
-                            double distance = 0;
+                            double distance = 0;            
 
 
-                            for (int e = 0; e < current_set.size(); e++) {
+                            for (int e = 0; e < current_set.size(); e++) {     //calcuates distance from c to d
                                 distance += pow((data[c][current_set[e]] - data[d][current_set[e]]), 2);
                             }
                             if (algorithm == 2) {
@@ -103,81 +106,66 @@ int main()
                             else {
                                 distance += pow((data[c][k] - data[d][k]), 2);
                             }
-                            distance = sqrt(abs(distance));
-                           //cout << "Distance = " << distance << endl;
-
-                            
-
-                            if (distance < nearest_neighbor_distance) {
-                                //cout << "new nearest = " << d << endl;
+                            distance = sqrt(abs(distance));                 
+                            if (distance < nearest_neighbor_distance) {     //if e is nearest sets variables
+                                
                                 nearest_neighbor_distance = distance;
                                 nearest_neighbor_location = d;
                                 nearest_neighbor_label = data[nearest_neighbor_location][0];
-                               //cout << "label = " << lable_object_to_classify <<   " nearest = " << nearest_neighbor_label << endl;
+                               
                             }
-                           // cout << "Ask if " << c << " = " << lable_object_to_classify << " is nearest neigbour with " << d << endl;
+                          
                         }
-
                     }        
                 if (lable_object_to_classify == nearest_neighbor_label) {
-                number_correctly_classfied++;}
-                   
-                }
-
-            //cout << "number correct = " << number_correctly_classfied << " number checked = " << data.size() << endl;
+                number_correctly_classfied++;}      //if guess is correct ++
+                } 
             accuracy = ((double)number_correctly_classfied/(double)((data.size())-1));
-           // cout << "Accuracy = " << accuracy << endl;
-                    if (accuracy > best_so_far) {
+                        
+                        if (accuracy > best_so_far) {       //if calculated accuracy is best new feature to add
                         best_so_far = accuracy;
                             feature_to_add = k;
-                            solution.clear();
-                            for (int z = 0; z < current_set.size(); z++) {
-                                solution.push_back(current_set[z]);
-                        }
+                           
                     }
                    
             }
 
         }
-        if (algorithm == 2) {
-            cout << "On level " << i << " remove feature " << feature_to_add << " from set\nAccuracy = "<< best_so_far << endl;
-            //vector<int>::iterator it = find(current_set.begin(), current_set.end(), feature_to_add);
-            //cout << "Erase " << *it << endl;
-
-            current_set.erase(find(current_set.begin(), current_set.end(), feature_to_add));
-            //cout << "set = { ";
-            //for (int i = 0; i < current_set.size(); i++) {
-            //    cout << current_set[i] << " ";
-            //}
-            //cout << " }\n";
+        if (algorithm == 2) {       //algorithm 1 adds feature to add 2 removes feature to add
+            cout << "On level " << i << " remove feature " << feature_to_add << " from set\nAccuracy = "<< best_so_far*100 << "%" << endl;
+            current_set.erase(find(current_set.begin(), current_set.end(), feature_to_add));    
         }
         else {
             current_set.push_back(feature_to_add);  
-            cout << "On level " << i <<  " added feature " <<  feature_to_add << " to set\nAccuracy = " << best_so_far << endl;
+            cout << "On level " << i <<  " added feature " <<  feature_to_add << " to set\nAccuracy = " << best_so_far*100 << "%" << endl;
         }
       
         if (best_so_far > bestacc) {
             bestacc = best_so_far;
             bestsetsize = i;
+            solution.clear();               //sets solution to current solution
+            for (int z = 0; z < current_set.size(); z++) {
+                solution.push_back(current_set[z]);
+            }
         }
     }
 
 
 
-    if (algorithm == 1) {
-        cout << "set = { ";
+    if (algorithm == 1) {               //outputs
+        cout << "Best set = { ";
         for (int i = 0; i < bestsetsize; i++) {
             cout << current_set[i] << " ";
         }
 
-        cout << "}\nwith an accuracy of " << bestacc << endl;
+        cout << "}\nwith an accuracy of " << bestacc*100 << "%" << endl;
     }
     else {
-        cout << "\nBest set = { ";
+        cout << "\nBest set = { ";      //outputs
         for (int i = 0; i < solution.size(); i++) {
             cout << solution[i] << " ";
         }
-        cout << "}\nwith an accuracy of " << bestacc << endl;
+        cout << "}\nwith an accuracy of " << bestacc*100 << "%" << endl;
     } 
 
     auto stop = high_resolution_clock::now();
